549beba5855e3cf68d1782e11b2f40edd3a283fb
istrebitelantox <geekpook@gmail.com>
2023-02-09 10:32:16 +0300

    WireMock FINALLY COMPLETED

--- src/test/java/Test.scala
+++ src/test/java/Test.scala
@@ -1,21 +1,32 @@
-import GetRequests._
 import PostRequest._
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.client.WireMock.{ok, urlEqualTo}
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration.options
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
-
 class Test extends Simulation{
+  val Port = 8080
+  val Host = "localhost"
+  val wireMock=new WireMock()
+  val wireMockServer = new WireMockServer()
   before {
     println("Simulation is about to start!")
+    wireMockServer.start()
+    WireMock.configureFor(Host,Port)
+    wireMockServer.stubFor(WireMock.post(urlEqualTo("/Account/v1/User")).willReturn(ok()))
   }
 
   after {
     println("Simulation is finished!")
+    wireMockServer.stop()
   }
-  val httpConf=http.baseUrl("http://localhost:8080")
-  val snc=scenario("Book Store").exec(postUser2).exec(getUserId)
+  val httpConf=http.baseUrl("http://localhost:8080/")
+  val snc=scenario("Book Store").exec(postUser)
+
   /*randomSwitch(
     (25,postAuto),
     (25,getUserId),
